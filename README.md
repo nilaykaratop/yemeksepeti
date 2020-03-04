3c9f24a42fe857cd108268015ddfd0e0bd557dd1
nilaykaratop <nilay.karatop@gmail.com>
2020-03-05 00:52:07 +0300

    first

---
+++ README.md
@@ -0,0 +1,16 @@
+# Yemek Sepeti
+[![N|Solid](https://www.bilgeadam.com/akademi/SiteAssets/BilgeAdam/Images/LogoAkademi.png)](https://www.bilgeadam.com/akademi/) 
+
+![yemeksepeti](https://user-images.githubusercontent.com/13505194/68115511-9882ac80-ff09-11e9-9199-ff8051fa81a9.png)
+
+
+
+* Kullanıcı, ComboBox'lara Kebap ve Pilav olarak 2 çeşit ekleyecektir
+* Kullanıcı, ComboBox'lardan aynı yemekleri seçemez, bir ana yemek bir ara yemek seçebilir.
+* Kullanıcı, Porsiyon bilgisini ve Yemek bilgisini boş geçemez
+* Kullanıcı, Porsiyon bilgisini bölümüne sadece sayısal veri girişi yapabilir.
+
+## Buton Seçenekleri
+* Kullanıcı, Sipariş iptal butonuna tıklarsa, form üzerinde yer alan tüm alanlar temizlenecek ve kullanıcıya bilgilendirme mesajı verilecektir.
+* Kullanıcı, Sipariş özeti butonuna tıklarsa, kullanının seçtiği yemekler formatlı bir şekilde ekranda kullanıcıya gösterilecektir.
+* Kullanıcı, Sipariş al butonuna tıklarsa, birim fiyatı ve porsiyon bilgisi hesaplanarak ödemesi gereken tutar ekranda yer alan ilgili alana yazdırılmalıdır ve form temizlenmelidir.
---
+++ WFA_YemekSepeti/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace WFA_YemekSepeti
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbBirinciYemek_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (cmbBirinciYemek.SelectedIndex == cmbIkinciYemek.SelectedIndex)
+            {
+                MessageBox.Show("lütfen farklı bir yemek seçiniz");
+                cmbBirinciYemek.SelectedItem = null;
+            }
+
+
+        }
+
+        private void cmbIkinciYemek_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbBirinciYemek.SelectedIndex == cmbIkinciYemek.SelectedIndex)
+            {
+                MessageBox.Show("LÜTFEN FARKLI BİR YEMEK SEÇİNİZ");
+
+                cmbIkinciYemek.SelectedItem = null;
+            }
+
+
+        }
+        int result;
+        int sonuc1;
+        int sonuc2;
+        private void btnSiparisAl_Click(object sender, EventArgs e)
+        {
+            if (cmbBirinciYemek.SelectedIndex < 0 || cmbIkinciYemek.SelectedIndex < 0)
+            {
+                MessageBox.Show("LÜTFEN YEMEK SEÇİNİZ");
+
+
+            }
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("LÜTFEN PORSİYON BİLGİSİ GİRİNİZ");
+            }
+           try
+            {
+                int sayi1 = Int32.Parse(textBox1.Text);
+                int sayi2 = Int32.Parse(textBox2.Text);
+                if (cmbBirinciYemek.SelectedIndex == 0)
+                {
+                    sonuc1 = sayi1 * 20;
+                }
+                else if (cmbBirinciYemek.SelectedIndex == 1)
+                {
+                    sonuc1 = sayi1 * 10;
+
+                }
+                if (cmbIkinciYemek.SelectedIndex == 0)
+                {
+                    sonuc2 = sayi2 * 20;
+                }
+                else if (cmbIkinciYemek.SelectedIndex == 1)
+                {
+                    sonuc2 = sayi2 * 10;
+                }
+                result = sonuc1 + sonuc2;
+
+                MessageBox.Show("Sipariş Tutarı: " + result.ToString() + " TL");
+                cmbIkinciYemek.Text = "";
+                cmbBirinciYemek.Text = "";
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            
+            catch
+            {
+                MessageBox.Show("");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void metroTile2_Click(object sender, EventArgs e)
+        {
+
+            cmbIkinciYemek.Text = "";
+            cmbBirinciYemek.Text = "";
+            textBox1.Clear();
+            textBox2.Clear();
+            MessageBox.Show("gidişiniz bizi üzüyor:(");
+        }
+
+        private void metroTile3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yemek Seçiminiz: " + cmbBirinciYemek.Text + " Porsiyon Tercihiniz:" + textBox1.Text + " Adet\n Yemek Seçimizin: " + cmbIkinciYemek.Text + " Porsiyon Tercihinz: " + textBox2.Text + " Adet");
+        }
+    }
+}
+
